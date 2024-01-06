a1d44c889ed717620391f3e460b69a2e7df238fe
ArturStarosta <artur.starosta@pwc.com>
2024-01-06 12:59:56 +0000

    update repo

--- src/cloud_storage_pwc/__init__.py
+++ src/cloud_storage_pwc/__init__.py
@@ -1,37 +1,49 @@
-from src.cloud_storage_pwc.Blob import Blob
-from src.cloud_storage_pwc.DataLake import DataLake
+from .Blob import Blob
+from .DataLake import DataLake
+
+
+if __name__ == '__main__':
+    pass
+
 
 
 
  
-def create_cloudstorage_reference(url:str,accessKey:str=None,tenantId:str=None,applicationId:str=None,applicationSecret:str=None):
+def create_cloudstorage_reference(accountname_or_url:str,accessKey:str=None,tenantId:str=None,applicationId:str=None,applicationSecret:str=None):
     """
-    Create a cloud storage reference object based on the provided URL and credentials.
+    Return a list of random ingredients as strings.
 
-    :param url: The URL of the cloud storage.
-    :type url: str
-    :param accessKey: The access key for the cloud storage (optional).
-    :type accessKey: str or None
-    :param tenantId: The tenant ID for authentication (optional).
-    :type tenantId: str or None
-    :param applicationId: The application ID for authentication (optional).
-    :type applicationId: str or None
-    :param applicationSecret: The application secret for authentication (optional).
-    :type applicationSecret: str or None
-    :return: The cloud storage reference object.
-    :rtype: DataLake or Blob
-    :raise Exception: If the URL is invalid or the cloud storage object cannot be created.
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
     """
-    create_object = None
-    if "dfs.core.windows.net" in url and url.startswith("https://"):
-        create_object = DataLake(url, accessKey, tenantId, applicationId, applicationSecret)
-    elif ("blob.core.windows.net" in url and url.startswith("https://")) or ("127.0.0.1:10000" in url and url.startswith("http://")):
-        create_object = Blob(url, accessKey, tenantId, applicationId, applicationSecret)
+    create_object =None
+    if "dfs.core.windows.net" in accountname_or_url and accountname_or_url.startswith("https://"):
+        create_object = DataLake(accountname_or_url,accessKey,tenantId,applicationId,applicationSecret)
+    elif ("blob.core.windows.net" in accountname_or_url and accountname_or_url.startswith("https://") ) or ("127.0.0.1:10000" in accountname_or_url and accountname_or_url.startswith("http://")):
+        create_object = Blob(accountname_or_url,accessKey,tenantId,applicationId,applicationSecret)
     else:
-        raise Exception("Invalid StorageAccount URL")
-    
-    if create_object is None:
-        raise Exception("Object does not exist")
+        try:
+            create_object = Blob(f"https://{accountname_or_url}.blob.core.windows.net/",accessKey,tenantId,applicationId,applicationSecret)
+            if create_object._check_is_blob():
+                raise Exception("DataLake not Blob")     
+        except Exception as e :
+            create_object_message =str(e)
+            try:
+                create_object = DataLake(f"https://{accountname_or_url}.dfs.core.windows.net/",accessKey,tenantId,applicationId,applicationSecret)
+            except Exception as e:
+                create_object_message = str(e)
+                
+                raise Exception(create_object_message)
+                
+        #if create_object is None:
+        #    try:
+        #        create_object = Blob(f"http://127.0.0.1:10000/{accountname_or_url}",accessKey,tenantId,applicationId,applicationSecret)
+        #    except:
+        #        create_object =None
+            
     
     return create_object
-    
+
